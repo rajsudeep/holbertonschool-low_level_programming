9aef9f9eeb49532f4e460fffad39712b7658f8c1
rajsudeep <sudrocks@gmail.com>
2019-06-19 18:08:46 +0000

    Print programming analogy

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Print programming analogy message
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char analStr[100] = "\"Programming is like building a multilingual puzzle";
+    puts(analStr);
+    return (0);
+}
