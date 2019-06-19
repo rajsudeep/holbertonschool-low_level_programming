0d9bef8b84fa4151b315c66c8944f139adc2385f
rajsudeep <sudrocks@gmail.com>
2019-06-19 20:46:47 +0000

    Try without variable

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,6 @@
  */
 int main(void)
 {
-	char analStr[100] = "\"Programming is like building a multilingual puzzle";
-	puts(analStr);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
