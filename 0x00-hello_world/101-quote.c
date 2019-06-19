8f39d6a288280ca1cb059b22bc420499828d8f10
rajsudeep <sudrocks@gmail.com>
2019-06-19 21:52:58 +0000

    Replace fprintf with write function)

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
  * main - Print Doras quote
  *
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
 	return (1);
 }
