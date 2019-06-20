df52bc1b73b2c5c15e3bfba4a62e10522c021355
rajsudeep <sudrocks@gmail.com>
2019-06-20 04:41:10 +0000

    Remove trailing spaces

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,13 @@
 #include <unistd.h>
 
 /**
- * main - Print Doras quote to stderr
+ * main - Print Dora quote
  *
- * Return: Always 0 (Success)
+ * Return: 1
  */
 int main(void)
 {
+
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
 	return (1);
 }
