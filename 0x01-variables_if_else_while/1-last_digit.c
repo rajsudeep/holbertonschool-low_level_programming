b236dfdc4893f44896f6be04c010692eda4c79b0
rajsudeep <sudrocks@gmail.com>
2019-06-20 20:05:47 +0000

    Reposition loops

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,10 +18,12 @@ int main(void)
 	if (lastDigit > 5)
 	{
 		printf("and is greater than 5\n");
-	} else if (lastDigit == 0)
+	}
+	else if (lastDigit == 0)
 	{
 		printf("and is 0\n");
-	} else if (lastDigit < 0)
+	}
+	else if (lastDigit < 0)
 	{
 		printf("and is less than 6 and not 0\n");
 	}
