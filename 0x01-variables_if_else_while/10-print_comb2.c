24921c88eea142e13eb6bc34636e30ee0efac00a
rajsudeep <sudrocks@gmail.com>
2019-06-21 00:34:42 +0000

    Print numbers 00 to 99

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Print the numbers from 00 to 99
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			putchar(i);
+			putchar(j);
+			if (i != '9' || j != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		j = '0';
+	}
+	putchar('\n');
+	return (0);
+}
