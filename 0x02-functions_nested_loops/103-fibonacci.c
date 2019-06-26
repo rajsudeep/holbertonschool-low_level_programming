3a015e599ec5c378a20ff8df541d3730991c8bf4
rajsudeep <sudrocks@gmail.com>
2019-06-26 04:31:04 +0000

    Sum of all even fibonacci numbers

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - print only even fibonacci numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	long f0 = 1;
+	long f1 = 2;
+	long i, fn, fn_2;
+
+	fn_2 = 0;
+	fn = 0;
+	while (f0 < 4000000)
+	{
+		if ((f1 % 2) == 0)
+			fn_2 += f1;
+		fn = f0 + f1;
+		f0 = f1;
+		f1 = fn;
+	}
+	printf("%ld\n", fn_2);
+	return (0);
+}
