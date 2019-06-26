ea498e5a59bb297780fef33d56a2e1e146ad88ad
rajsudeep <sudrocks@gmail.com>
2019-06-26 00:43:24 +0000

    Print the sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * print_sign - prints the sign of a number to stdout
+ * @n: number to test
+ *
+ * Return: 1, 0, -1 depending on what value n is (positive, zero, negative)
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
