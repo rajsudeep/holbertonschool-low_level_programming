c60fa87db015e5f2a0a62daba79440f5c3b787f6
rajsudeep <sudrocks@gmail.com>
2019-06-26 01:52:38 +0000

    Add error test

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,5 +1,5 @@
-#ifndef _HOLBERTON_
-#define _HOLBERTON_
+#ifndef _HOLBERTON_H
+#define _HOLBERTON_H
 
 int _putchar(char c);
 void print_alphabet(void);
@@ -15,4 +15,4 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 
-#endif
+#endif /*HOLBERTON_H*/
