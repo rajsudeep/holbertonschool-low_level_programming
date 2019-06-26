e57938990fae57a27cc6e012e0326abe8652f448
rajsudeep <sudrocks@gmail.com>
2019-06-26 05:40:48 +0000

    Sum relative to fn

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -13,10 +13,11 @@ int main(void)
 
 	fn_2 = 2;
 	fn = 0;
-	while ((f0 < 4000000) && (f1 < 4000000))
+	while (fn < 4000000)
 	{
-		if ((f1 % 2) == 0)
-			fn_2 += f1;
+		fn = f0 + f1;
+		if ((fn % 2) == 0)
+			fn_2 += fn;
 		fn = f0 + f1;
 		f0 = f1;
 		f1 = fn;
