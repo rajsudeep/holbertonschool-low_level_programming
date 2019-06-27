55651ed2b8f59731694c34cc59d9fabbeff466cc
rajsudeep <sudrocks@gmail.com>
2019-06-27 20:21:19 +0000

    Draw a diagnol line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - draws a straight line
+ * @n: size of line
+ *
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+	int spaces = 0;
+
+	if (n <= 0)
+		return;
+	for (i = 0; i < n; i++)
+	{
+		spaces++;
+		for (j = 0; j < spaces; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
