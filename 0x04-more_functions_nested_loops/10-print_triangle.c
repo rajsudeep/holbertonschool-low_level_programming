fbcac1b050177e4aab5ea5bf90b7bf5230dd5ef7
rajsudeep <sudrocks@gmail.com>
2019-06-27 20:23:05 +0000

    Print triangle out of pound signs

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * print_triangle - prints a # triangle to stdout
+ * @size: size of triangle
+ *
+ */
+void print_triangle(int size)
+{
+	int i;
+	int j;
+	int spaces = 0;
+
+	if (size <= 0)
+		return;
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			spaces = (size - 1) - i;
+			if (j < spaces)
+				_putchar(' ');
+			else
+				_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
