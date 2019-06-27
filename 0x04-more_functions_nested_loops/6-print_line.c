b0cdad344ee8211c765e02546d7d49b5b84e12a5
rajsudeep <sudrocks@gmail.com>
2019-06-27 20:20:36 +0000

    Draw a straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_line - draws a straight line
+ * @n: size of line
+ *
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+		return;
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
