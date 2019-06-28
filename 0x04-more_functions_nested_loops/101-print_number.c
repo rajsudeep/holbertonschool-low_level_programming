cbb5b92318e52a1f0f01c9eaa1874079cf2a0d16
rajsudeep <sudrocks@gmail.com>
2019-06-28 02:43:53 +0000

    Readd the print number script for the third time:

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,17 +2,16 @@
 #include <stdio.h>
 
 void print_number(int n);
+int count_int(int n);
 int _abs(int n);
 int _pow(int x, int y);
-int count_int(int n);
 
 /**
- * count_int - counts number of digits
- *
- * @n: integer to count from
- *
- * Return: number of digits
+ * count_int - counts the number of digits in an integer
+ * @n: input number from 101-main.c
+ * Return: The number of digits of the input number
  */
+
 int count_int(int n)
 {
 	int count = 0;
@@ -26,12 +25,11 @@ int count_int(int n)
 }
 
 /**
- * _abs - create absolute value
- *
- * @n: integer to absolute
- *
- * Return: value absoluted
+ * _abs - calculates the absolute value of a number
+ * @n: input from 101-main.c
+ * Return: A positive value
  */
+
 int _abs(int n)
 {
 	if (n < 0)
@@ -45,45 +43,47 @@ int _abs(int n)
 }
 
 /**
- * _pow - computes the power of a int
- *
- * @x: int to power
- * @y: amount of power
- *
- * Return: the powered value
+ * _pow - calculates the power of a number
+ * @x: the base of number
+ * @y: the power of the number
+ * Return: x
  */
+
 int _pow(int x, int y)
 {
-	int i;
+	int temp;
 
-	for (i = 0; i < y; i++)
-		x *= y;
-	return (x);
+	if (y == 0)
+		return (1);
+	temp = _pow(x, y / 2);
+	if (y % 2 == 0)
+		return (temp * temp);
+	else
+		return (x * temp * temp);
 }
 
 /**
- * print_number - print integer
- *
- * @n: integer to print
- *
+ * print_number - prints the input number as an integer
+ * @n: input from 101-main.c
  */
+
 void print_number(int n)
 {
 	int digits = count_int(n);
-	int i;
 	int x;
 	int currDigit;
+	int i;
 
 	if (n < 0)
-		putchar('-');
+		_putchar('-');
 
 	n = _abs(n);
 
-	putchar('0' + (n / (_pow(10, digits - 1))));
+	_putchar('0' + (n / (_pow(10, digits - 1))));
 	for (i = 2; i <= digits; i++)
 	{
 		x = _pow(10, (digits - i));
 		currDigit = (n / x) % 10;
-		putchar(currDigit + '0');
+		_putchar(currDigit + '0');
 	}
 }
