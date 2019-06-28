9732af1c83844fdddce68e886636a9c99c4f34de
rajsudeep <sudrocks@gmail.com>
2019-06-28 01:52:21 +0000

    Print integer numbers without using any set print functions

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,92 @@
+#include "holberton.h"
+#include <stdio.h>
+
+void print_number(int n);
+int _abs(int n);
+int _pow(int x, int y);
+int count_int(int n);
+
+/**
+ * count_int - counts number of digits
+ *
+ * @n: integer to count from
+ *
+ * Return: number of digits
+ */
+int count_int(int n)
+{
+	int count = 0;
+
+	while (n != 0)
+	{
+		n /= 10;
+		++count;
+	}
+	return (count);
+}
+
+/**
+ * _abs - create absolute value
+ *
+ * @n: integer to absolute
+ *
+ * Return: value absoluted
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-1 * n);
+	}
+	else
+	{
+		return (n);
+	}
+}
+
+/**
+ * _pow - computes the power of a int
+ *
+ * @x: int to power
+ * @y: amount of power
+ *
+ * Return: the powered value
+ */
+int _pow(int x, int y)
+{
+	int result = 1;
+
+	while (y != 0)
+	{
+		result *= x;
+		--y;
+	}
+	return (result);
+}
+
+/**
+ * print_number - print integer
+ *
+ * @n: integer to print
+ *
+ */
+void print_number(int n)
+{
+	int digits = count_int(n);
+	int i;
+	int x;
+	int currDigit;
+
+	if (n < 0)
+		putchar('-');
+
+	n = _abs(n);
+
+	putchar('0' + (n / (_pow(10, digits - 1))));
+	for (i = 2; i <= digits; i++)
+	{
+		x = _pow(10, (digits - i));
+		currDigit = (n / x) % 10;
+		putchar(currDigit + '0');
+	}
+}
