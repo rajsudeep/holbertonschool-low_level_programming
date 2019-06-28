a9845d18d10278e3e197bd69d4e5c9cb9237177f
rajsudeep <sudrocks@gmail.com>
2019-06-28 20:34:31 +0000

    Change input integer to unsigned to print out

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -69,24 +69,22 @@ int _pow(int x, int y)
 
 void print_number(int n)
 {
-	int digits;
-	int i;
+	int digits = count_int(n);
 	int x;
-	int lastDigit;
 	int currDigit;
+	int i;
+	unsigned int un_n;
 
 	if (n < 0)
 		_putchar('-');
-	lastDigit = _abs(n % 10);
-	n = _abs(n / 10);
-	digits = count_int(n);
 
-	_putchar('0' + (n / (_pow(10, digits - 1))));
+	un_n = _abs(n);
+
+	_putchar('0' + (un_n / (_pow(10, digits - 1))));
 	for (i = 2; i <= digits; i++)
 	{
 		x = _pow(10, (digits - i));
-		currDigit = (n / x) % 10;
+		currDigit = (un_n / x) % 10;
 		_putchar(currDigit + '0');
 	}
-	_putchar(lastDigit + '0');
 }
