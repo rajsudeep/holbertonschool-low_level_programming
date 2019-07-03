2f5e30b8725c350d90c260c7ff41849264432fa4
rajsudeep <sudrocks@gmail.com>
2019-07-03 05:46:10 +0000

    Add random valid password generator

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - create random keys thats ascii values sum to 2772
+ *
+ *
+ * Return: On success 1.
+ *
+ */
+int main(void)
+{
+	char asciivals[63] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	char key[100];
+	int count = 0;
+	int i = 0;
+	int val = 0;
+
+	srand(time(NULL));
+	for (; count < (2772 - 127); i++)
+	{
+		val = rand() % 62;
+		key[i] = asciivals[val];
+		count += key[i];
+	}
+	val = 2772 - count;
+	key[i] = val;
+	i++;
+	key[i] = '\0';
+	printf("%s", key);
+	return (0);
+}
