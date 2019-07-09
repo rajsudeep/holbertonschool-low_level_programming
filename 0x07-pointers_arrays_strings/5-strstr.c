4b6d1fd7ab3099421121af6676149566fc114d8e
rajsudeep <sudrocks@gmail.com>
2019-07-09 18:15:21 +0000

    Add task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * *_strstr - finds the first occurrence of the
+ * substring needle in the string haystack
+ *
+ * @haystrack: input string
+ * @needle: desired substring
+ * Return: pointer to the beginning of the located
+ * substring, else NULL
+ *
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (needle[0] == haystack[i])
+		{
+			for (j = 0; needle[j] != '\0'; j++)
+			{
+				if (haystack[i + j] != needle[j])
+					break;
+				if (needle[j + 1] == '\0')
+					return (haystack + i);
+			}
+		}
+		j = 0;
+	}
+	return (NULL);
+}
