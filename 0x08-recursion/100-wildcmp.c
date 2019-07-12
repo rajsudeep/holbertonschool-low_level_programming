38e1d345788685f930066e95030f98a2d50aeb44
rajsudeep <sudrocks@gmail.com>
2019-07-12 06:30:35 +0000

    Create case for main-main.c

---
+++ 0x08-recursion/100-wildcmp.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+
+/**
+ * wildcmp - compare a file to a file with potential
+ * wildcard notation
+ *
+ * @s1: file name
+ * @s2: file name with wildcard or other notation
+ * Return: if similar 1, else 0
+ *
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && (*s2 == '\0' || *(s2 + 1) == '\0'))
+		return (1);
+	else if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	else if (*s1 != *s2 && *s2 != '*')
+		return (0);
+	else if (*s1 != *s2 && *s2 != '*' && *s1 == '-')
+		return (1);
+	else if (*s1 != *s2 && *s2 == '*')
+	{
+		if (*s1 == *(s2 + 1))
+			return (wildcmp(s1, s2 + 1));
+		else if (*s1 != *s2 + 1)
+		{
+			if (*(s2 + 1) == '*')
+				return (wildcmp(s1, s2 + 1));
+			else
+				return (wildcmp(s1 + 1, s2));
+		}
+		else
+			return (0);
+	}
+	else
+		return (0);
+}
