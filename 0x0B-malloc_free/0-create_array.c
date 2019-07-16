bc503a37ba1f433c115e5dba3af76f9aa9892009
rajsudeep <sudrocks@gmail.com>
2019-07-16 20:50:14 +0000

    Check for null earlier

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,13 +16,10 @@ char *create_array(unsigned int size, char c)
 	char *arr;
 	unsigned int i;
 
-	if (size == 0)
-		return (NULL);
 	arr = (char *) malloc(sizeof(char) * size);
+	if (size == 0 || arr == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 		*(arr + i) = c;
-	if (arr == NULL)
-		return (NULL);
-	else
-		return (arr);
+	return (arr);
 }
