cd9519341da5f422a6df3af8d6bd07b46c65bef9
rajsudeep <sudrocks@gmail.com>
2019-07-16 20:45:33 +0000

    Add task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - creates a duplicate string
+ *
+ * @str: input string to duplicate
+ * Return: a pointer to duplicate string
+ *
+ */
+char *_strdup(char *str)
+{
+	char *new;
+	int i, strlen;
+
+	if (str == NULL)
+		return (NULL);
+	strlen = 0;
+	while (str[strlen] != '\0')
+		strlen++;
+	new = malloc(sizeof(char) * (strlen + 1));
+	for (i = 0; i < strlen + 1; i++)
+		*(new + i) = *(str + i);
+	return (new);
+}
