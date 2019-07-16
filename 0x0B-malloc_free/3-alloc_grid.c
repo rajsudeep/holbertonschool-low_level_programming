6f16bcccb619c77339c602dd16cb5a163693b028
rajsudeep <sudrocks@gmail.com>
2019-07-16 22:43:52 +0000

    Change position of null conditional

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,13 +18,10 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 	arr = (int **)malloc(sizeof(int *) * width);
+	for (i = 0; i < width; i++)
+		arr[i] = (int *)malloc(sizeof(int) * height);
 	if (arr == NULL)
 		return (NULL);
-	for (i = 0; i < width; i++)
-		if (arr[i] == NULL)
-			return (NULL);
-		arr[i] = (int *)malloc(sizeof(int) * height);
-
 	for (i = 0; i < width; i++)
 		for (j = 0; j < height; j++)
 			arr[i][j] = 0;
