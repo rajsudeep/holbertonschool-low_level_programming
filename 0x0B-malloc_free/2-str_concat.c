05e64318104d0d110d63b85b7bb05d14672a6560
rajsudeep <sudrocks@gmail.com>
2019-07-16 21:39:30 +0000

    Add s1 and s1 null cases

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,6 +15,10 @@ char *str_concat(char *s1, char *s2)
 	char *sc;
 	unsigned int i, j, len1, len2, lenc;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	len1 = 0;
 	while (s1[len1] != '\0')
 		len1++;
