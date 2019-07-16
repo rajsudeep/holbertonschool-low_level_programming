d4ecd5278b6d37a21fdd595cdafb2a18f67a1444
rajsudeep <sudrocks@gmail.com>
2019-07-16 23:52:15 +0000

    Replace height and width

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,9 +18,9 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 	arr = (int **)malloc(sizeof(int *) * width);
-	for (i = 0; i < width; i++)
+	for (i = 0; i < height; i++)
 	{
-		arr[i] = (int *)malloc(sizeof(int *) * height);
+		arr[i] = malloc(sizeof(int) * width);
 		if (arr[i] == NULL)
 		{
 			while (i >= 0)
@@ -34,8 +34,8 @@ int **alloc_grid(int width, int height)
 	}
 	if (arr == NULL)
 		return (NULL);
-	for (i = 0; i < width; i++)
-		for (j = 0; j < height; j++)
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
 			arr[i][j] = 0;
 	return (arr);
 }
