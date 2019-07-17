be01f53cce8717073b57fa3d3bbaed49a6353c81
rajsudeep <sudrocks@gmail.com>
2019-07-17 00:01:15 +0000

    Replace malloc width with height

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,7 +17,7 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	arr = (int **)malloc(sizeof(int *) * width);
+	arr = (int **)malloc(sizeof(int *) * height);
 	if (arr == NULL)
 		return (NULL);
 	for (i = 0; i < height; i++)
