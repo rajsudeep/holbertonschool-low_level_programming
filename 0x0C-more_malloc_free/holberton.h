9d220cf07dd4ad812097fad1b7d4bb95dc859d52
rajsudeep <sudrocks@gmail.com>
2019-07-18 18:43:29 +0000

    Add prototypes for task 0 and 1

---
+++ 0x0C-more_malloc_free/holberton.h
@@ -0,0 +1,9 @@
+#ifndef _HOLBERTON_
+#define _HOLBERTON_
+
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+
+#endif
