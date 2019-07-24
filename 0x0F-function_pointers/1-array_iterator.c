eebbdc23f43a11a9c161ae3fc9927c34c9dd8286
rajsudeep <sudrocks@gmail.com>
2019-07-24 19:38:09 +0000

    Add task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * array_iterator - run through a function using
+ * various parameters
+ *
+ * @array: set of parameter inputs
+ * @size: size of parameter array
+ * @action: function to execute
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (action)
+		for (i = 0; i < size; i++)
+			action(array[i]);
+}
