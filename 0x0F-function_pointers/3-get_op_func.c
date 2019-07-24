b0715ce546e6700e1827aaca5986912f4b26ab68
rajsudeep <sudrocks@gmail.com>
2019-07-24 22:38:53 +0000

    Compare strings in order to deal with multiple operation case

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "3-calc.h"
 
 /**
@@ -25,7 +26,7 @@ int (*get_op_func(char *s))(int, int)
 	i = 0;
 	while (ops[i].op)
 	{
-		if (*s == *(ops[i]).op)
+		if (!strcmp(s, ops[i].op))
 			return (ops[i].f);
 		i++;
 	}
