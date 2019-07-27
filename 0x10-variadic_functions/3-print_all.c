d09a8f358705e2b4cc7bcd2bd3852371a13c5aec
rajsudeep <sudrocks@gmail.com>
2019-07-27 20:21:31 +0000

    Add task 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,88 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_c - prints the given character values
+ *
+ * @c: list of character arguments to print
+ *
+ */
+void print_c(va_list c)
+{
+	printf("%c", va_arg(c, int));
+}
+
+/**
+ * print_i - prints the given integer values
+ *
+ * @i: list of integer arguments to print
+ *
+ */
+void print_i(va_list i)
+{
+	printf("%i", va_arg(i, int));
+}
+
+/**
+ * print_f - prints the given float values
+ *
+ * @f: list of float arguments to print
+ *
+ */
+void print_f(va_list f)
+{
+	printf("%f", va_arg(f, double));
+}
+
+/**
+ * print_s - prints the given strings
+ *
+ * @s: list of string arguments to print
+ *
+ */
+void print_s(va_list s)
+{
+	printf("%s", va_arg(s, char *));
+}
+
+/**
+ * print_all - prints the values that exist relative
+ * corresponding and valid specifiers given
+ *
+ * @format: given format string starting with string
+ * of specifiers
+ *
+ */
+void print_all(const char * const format, ...)
+{
+	t_printarg args[] = {
+		{"c", print_c},
+		{"i", print_i},
+		{"f", print_f},
+		{"s", print_s},
+		{NULL, NULL}
+	};
+	va_list arguments;
+	int i, j;
+
+	va_start(arguments, format);
+	i = 0;
+	while (format && *(format + i) != '\0')
+	{
+		j = 0;
+		while (args[j].fmt)
+		{
+			if (*(format + i) == *(args[j]).fmt)
+			{
+				args[j].f(arguments);
+				if (*(format + i + 1) != '\0')
+					printf(", ");
+			}
+			j++;
+		}
+		i++;
+	}
+	va_end(arguments);
+	printf("\n");
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,12 @@
 #ifndef _VARIADIC_FUNCTIONS_
 #define _VARIADIC_FUNCTIONS_
 
+#include <stdarg.h>
+typedef struct printarg
+{
+	char *fmt;
+	void (*f)(va_list);
+} t_printarg;
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
