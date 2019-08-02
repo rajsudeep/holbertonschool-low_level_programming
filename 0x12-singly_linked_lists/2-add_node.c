2141c2c5ba223b3ceadb86892335889d381a3f23
rajsudeep <sudrocks@gmail.com>
2019-08-02 00:06:28 +0000

    Use homebrewed strlen

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,6 +2,23 @@
 #include "lists.h"
 
 /**
+ * _strlen - finds and returns length of string
+ * @s: string value
+ *
+ * Return: length of string
+ */
+int _strlen(const char *s)
+{
+	int length;
+
+	for (length = 0; s[length] != '\0'; length++)
+	{
+	}
+
+	return (length);
+}
+
+/**
  * add_node - adds a new node to the beginning of the list
  * @head: pointer that points to the beginning of a list
  * @str: string value to add to new node
@@ -16,7 +33,7 @@ list_t *add_node(list_t **head, const char *str)
 	if (temp == NULL)
 		return (NULL);
 	temp->str = strdup(str);
-	temp->len = 0;
+	temp->len = _strlen(str);
 	temp->next = *head;
 	*head = temp;
 	return (temp);
