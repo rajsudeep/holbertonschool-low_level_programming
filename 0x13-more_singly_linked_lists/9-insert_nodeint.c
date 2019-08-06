ed203d38b578d8f946b3ad36dbb94c6d2e58ff97
rajsudeep <sudrocks@gmail.com>
2019-08-06 19:30:21 +0000

    Refactor conditional statements

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,18 +14,19 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (head == NULL)
 		return (NULL);
-
-	if (*head != NULL && idx)
-		return (insert_nodeint_at_index(&(*head)->next, idx - 1, n));
-	if (idx)
-		return (NULL);
-
+	if (idx != 0)
+	{
+		if (*head != NULL)
+			return (insert_nodeint_at_index(&(*head)->next,
+							idx - 1, n));
+		else
+			return (NULL);
+	}
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
 	new->next = *head;
 	*head = new;
-
 	return (new);
 }
