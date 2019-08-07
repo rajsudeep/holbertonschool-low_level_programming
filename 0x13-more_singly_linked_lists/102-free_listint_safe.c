49be850b033ed104a658ed51a4ae75b4daad7132
rajsudeep <sudrocks@gmail.com>
2019-08-07 04:46:18 +0000

    Add task 102

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a list (including loops)
+ * @head: list of numbers
+ *
+ * Return: Number of nodes freed from the list
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *tmpCur;
+	int len = 0;
+
+	if (h != NULL)
+	{
+		for (; *h != NULL; len++)
+		{
+			if (*h > (*h)->next)
+			{
+				tmpCur = *h;
+				*h = (*h)->next;
+				free(tmpCur);
+			}
+			else
+			{
+				free(*h);
+				*h = NULL;
+			}
+		}
+	}
+	return (len);
+}
