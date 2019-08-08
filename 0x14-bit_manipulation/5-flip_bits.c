4def210e48488c1f158b33aca7b7f2e4bf4f54bc
rajsudeep <sudrocks@gmail.com>
2019-08-08 20:00:39 +0000

    Change size of length

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -34,7 +34,7 @@ int get_bit(unsigned long int n, unsigned int index)
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int count = 0;
-	unsigned int len = sizeof(unsigned long int) * 8 - 1, i;
+	unsigned int len = sizeof(unsigned long int) * 8, i;
 
 	for (i = 0; i < len; i++)
 		if (get_bit(n, i) ^ get_bit(m, i))
