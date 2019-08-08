eeee591c6109c5b475e22eb0186f56318142c62d
rajsudeep <sudrocks@gmail.com>
2019-08-08 19:07:00 +0000

    Refactor conditional statement

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -17,10 +17,7 @@ int get_bit(unsigned long int n, unsigned int index)
 	{
 		k = n >> i;
 		if (i == index)
-		{
-			(k & 1) ? (bit = 1) : (bit = 0);
-			return (bit);
-		}
+			(k & 1) ? (bit = 1) : (bit = 0), return (bit);
 	}
 	return (-1);
 }
