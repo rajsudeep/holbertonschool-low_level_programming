0c66aa4e72512f23d0ce872506c9a268a9be490b
rajsudeep <sudrocks@gmail.com>
2019-08-13 20:31:00 +0000

    Fix betty warning errors

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -28,12 +28,12 @@ int append_text_to_file(const char *filename, char *text_content)
 	int fd, numWrite, text_len;
 
 	if (!(filename))
-	    return (-1);
+		return (-1);
 	fd = open(filename, O_APPEND | O_RDWR);
 	if (fd == -1)
 		return (-1);
 	if (!(text_content))
-	    return (1);
+		return (1);
 	text_len = _strlen(text_content);
 	numWrite = write(fd, text_content, text_len);
 	close(fd);
