c448e56610a608b284368da1969ef28d573299cd
rajsudeep <sudrocks@gmail.com>
2019-09-16 03:02:00 +0000

    Check if referenced pointer being tested

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -11,7 +11,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *trash, *tmp = *head;
 
-	if (head == NULL)
+	if (head == NULL || *head == NULL)
 		return (-1);
 	if (index == 0)
 	{
