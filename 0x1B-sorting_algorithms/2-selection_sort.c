980dcb6ac1c09c7526d0d2c5fc5581a5f9c8433e
rajsudeep <sudrocks@gmail.com>
2019-10-28 17:57:03 +0000

    Remove unneeded swaps

--- 0x1B-sorting_algorithms/2-selection_sort.c
+++ 0x1B-sorting_algorithms/2-selection_sort.c
@@ -12,15 +12,18 @@ void selection_sort(int *array, size_t size)
 
 	if (array == NULL || size < 1)
 		return;
-	for (i = 0; i < size - 1; i ++)
+	for (i = 0; i < size - 1; i++)
 	{
 		min = i;
 		for (j = i + 1; j < size; j++)
 			if (array[j] < array[min])
 				min = j;
-		tmp = array[min];
-		array[min] = array[i];
-		array[i] = tmp;
-		print_array(array, size);
+		if (i != min)
+		{
+			tmp = array[min];
+			array[min] = array[i];
+			array[i] = tmp;
+			print_array(array, size);
+		}
 	}
 }
