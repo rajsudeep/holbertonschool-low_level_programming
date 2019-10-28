a19ce1f3301c768ac1ea515feb3054310132b4af
rajsudeep <sudrocks@gmail.com>
2019-10-28 03:28:44 +0000

    Add task 2

---
+++ 0x1B-sorting_algorithms/2-selection_sort.c
@@ -0,0 +1,26 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts an array of integers in ascending order
+ * @array: array of integers
+ * @size: size of array
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min;
+	int tmp;
+
+	if (array == NULL || size < 1)
+		return;
+	for (i = 0; i < size - 1; i ++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+			if (array[j] < array[min])
+				min = j;
+		tmp = array[min];
+		array[min] = array[i];
+		array[i] = tmp;
+		print_array(array, size);
+	}
+}
