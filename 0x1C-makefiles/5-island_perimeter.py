1b32e6d390529b992183d222fa31d7b11b06ae1d
rajsudeep <sudrocks@gmail.com>
2019-11-16 01:33:53 +0000

    Add descrip

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,11 +1,12 @@
 #!usr/bin/python3
-""" Module that finds the perimeter of an island """
+"""
+Module that finds the perimeter of an island
+"""
 
 
 def island_perimeter(grid):
     """ Finds the perimeter of an island """
     count = 0
-
     for j, r in enumerate(grid):
         for i, c in enumerate(r):
             if c == 1:
