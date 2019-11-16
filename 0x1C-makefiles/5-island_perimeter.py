c0eccafc5e0fee076d566f5934bcba05b25e0a5d
rajsudeep <sudrocks@gmail.com>
2019-11-16 01:32:01 +0000

    Add task 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,19 @@
+#!usr/bin/python3
+""" Module that finds the perimeter of an island """
+
+
+def island_perimeter(grid):
+    count = 0
+
+    for j, r in enumerate(grid):
+        for i, c in enumerate(r):
+            if c == 1:
+                if j == 0 or grid[j - 1][i] == 0:
+                    count += 1
+                if i == 0 or grid[j][i - 1] == 0:
+                    count += 1
+                if j == len(grid) - 1 or grid[j + 1][i] == 0:
+                    count += 1
+                if i == len(r) - 1 or grid[j][i + 1] == 0:
+                    count += 1
+    return count
