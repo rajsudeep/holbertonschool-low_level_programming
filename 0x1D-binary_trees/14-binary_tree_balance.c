17319adab50b350d790c20df0eb0c6628c77927b
rajsudeep <sudrocks@gmail.com>
2019-12-07 03:09:59 +0000

    Remove whitespace

--- 0x1D-binary_trees/14-binary_tree_balance.c
+++ 0x1D-binary_trees/14-binary_tree_balance.c
@@ -8,17 +8,17 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-        size_t l = 0, r = 0;
+	size_t l = 0, r = 0;
 
-        if (tree != NULL)
-        {
-                l = binary_tree_height(tree->left) + 1;
-                r = binary_tree_height(tree->right) + 1;
-        }
-        if (l >= r)
-                return (l);
-        else
-                return (r);
+	if (tree != NULL)
+	{
+		l = binary_tree_height(tree->left) + 1;
+		r = binary_tree_height(tree->right) + 1;
+	}
+	if (l >= r)
+		return (l);
+	else
+		return (r);
 }
 
 /**
