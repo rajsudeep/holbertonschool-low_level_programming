ff59dd3e573ff1cee21f9facd5aca4836036b4ba
rajsudeep <sudrocks@gmail.com>
2019-12-07 02:21:28 +0000

    Consistency makes the world go round

--- 0x1D-binary_trees/9-binary_tree_height.c
+++ 0x1D-binary_trees/9-binary_tree_height.c
@@ -10,7 +10,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t l = 0, r = 0;
 
-	if (tree)
+	if (tree != NULL)
 	{
 		l = tree->left ? binary_tree_height(tree->left) + 1 : 0;
 		r = tree->right ? binary_tree_height(tree->right) + 1 : 0;
